e86630eaf0101df3cd2e5093dc3533c9c707837c
Martin Delille <martin@phonations.com>
2019-01-09 15:52:13 +0100

    failing enum class test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
-#include <QCoreApplication>
+#include "bandit/bandit.h"
+
+using namespace bandit;
+using namespace snowhouse;
+
+enum class Animal {
+	Dog,
+	Horse,
+	Cat
+};
+
+go_bandit([]() {
+	describe("enum class", [&]() {
+		it("fails displaying enum class value", [&] {
+			Animal dog = Animal::Dog;
+			Animal cat = Animal::Cat;
+			AssertThat(dog, Equals(cat));
+		});
+	});
+});
 
 int main(int argc, char *argv[])
 {
-	QCoreApplication a(argc, argv);
-
-	return a.exec();
+	return bandit::run(argc, argv);
 }
